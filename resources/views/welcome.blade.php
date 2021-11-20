98d7055bdff7974f996c6a7905e8101168063695
Nibir <nibirahmed9611@gmail.com>
2021-11-21 01:30:28 +0600

    Favicon added on all pages including welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="title" content="Code Ecstasy Course Management System">
+    <meta name="description" content="Code Ecstasy Course Management System is a course management system where you one can handle payments attendance course exams etc. Grab your copy now!">
+    <meta name="author" content="https://codecstasy.com">
+    <link rel="shortcut icon" href="{{ asset("images/favicon/favicon.png") }}" type="image/x-icon">
     <title>CE CMS</title>
 
     <!-- Bootstrap core CSS -->
