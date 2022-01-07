8afe6099400402b057c281316d88470e680cff48
Nibir <nibirahmed9611@gmail.com>
2022-01-07 16:36:56 +0600

    Dashboard Title changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="title" content="Code Ecstasy Course Management System">
-    <meta name="description" content="Code Ecstasy Course Management System is a course management system where you one can handle payments attendance course exams etc. Grab your copy now!">
+    <meta name="title" content="{{ env("APP_NAME") }}">
+    <meta name="description" content="{{ env("APP_NAME") }} is built by Code Ecstasy, It's awesome right? Grab your copy now!">
     <meta name="author" content="https://codecstasy.com">
     <link rel="shortcut icon" href="{{ asset("images/favicon/favicon.png") }}" type="image/x-icon">
-    <title> {{ env("APP_NAME") }} - CE CMS</title>
+    <title> {{ env("APP_NAME") }} - Code Ecstasy</title>
 
     <!-- Bootstrap core CSS -->
     <link href="{{ asset("assets/frontpage/bootstrap.min.css") }}" rel="stylesheet">
