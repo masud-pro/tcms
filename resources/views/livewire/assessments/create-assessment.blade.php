170611c9d6932260086adf924cbe12ad8b1481d9
Nibir Ahmed <nibirahmed9611@gmail.com>
2023-01-31 15:36:40 +0600

    assessment left

--- resources/views/livewire/assessments/create-assessment.blade.php
+++ resources/views/livewire/assessments/create-assessment.blade.php
@@ -127,31 +127,31 @@
                 ],
                 toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
                 file_picker_callback : function(callback, value, meta) {
-                var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-                var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+                    var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+                    var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
 
-                var cmsURL = editor_config.path_absolute + 'laravel-filemanager?editor=' + meta.fieldname;
-                if (meta.filetype == 'image') {
-                    cmsURL = cmsURL + "&type=Images";
-                } else {
-                    cmsURL = cmsURL + "&type=Files";
-                }
-
-                tinyMCE.activeEditor.windowManager.openUrl({
-                    url : cmsURL,
-                    title : 'Filemanager',
-                    width : x * 0.8,
-                    height : y * 0.8,
-                    resizable : "yes",
-                    close_previous : "no",
-                    onMessage: (api, message) => {
-                    callback(message.content);
+                    var cmsURL = editor_config.path_absolute + 'laravel-filemanager?editor=' + meta.fieldname;
+                    if (meta.filetype == 'image') {
+                        cmsURL = cmsURL + "&type=Images";
+                    } else {
+                        cmsURL = cmsURL + "&type=Files";
                     }
-                });
-            }
-        };
 
-        tinymce.init(editor_config);
+                    tinyMCE.activeEditor.windowManager.openUrl({
+                        url : cmsURL,
+                        title : 'Filemanager',
+                        width : x * 0.8,
+                        height : y * 0.8,
+                        resizable : "yes",
+                        close_previous : "no",
+                        onMessage: (api, message) => {
+                        callback(message.content);
+                        }
+                    });
+                }
+            };
+
+            tinymce.init(editor_config);
 
             
 
